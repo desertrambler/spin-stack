412f5427b3c8bc598c0820359e04902b86e627b4
Clayton <ccrenshaw2012@gmail.com>
2025-07-19 01:22:46 -0500

    added sqlite3 db

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import os
+import sqlite3
 
 app = Flask(__name__)
 
@@ -50,6 +52,20 @@ def show_add_record_modal():
             </div>
             '''
     return html
+
+DATABASE = os.path.join(app.root_path, 'database.db')
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
